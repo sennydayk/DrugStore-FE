c9303fc545494c589ab48ccd38c1b6fe59aadaec
Seongseri <tjdtpfl@gmail.com>
2024-06-20 19:07:31 +0900

    feat: 로그아웃 버튼 클릭 시 확인 창 추가 및 로그인 여부에 따라 이모티콘 변경

--- src/App.tsx
+++ src/App.tsx
@@ -15,26 +15,29 @@ import Mainpage from "./pages/Mainpage/Mainpage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Categorypage from "./pages/Categorypage/Categorypage";
 import { Detailpage } from "./pages/Detailpage/Detailpage";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const App: React.FC = () => (
-  <div className="App">
-    <Header />
-    <Routes>
-      <Route path="/" element={<Mainpage />} />
-      <Route path="main/category/:categoryId" element={<Categorypage />} />
-      <Route path="/detail/:productid" element={<Detailpage />} />
-      <Route path="/auth/login" element={<LoginPage />} />
-      <Route path="/find-email" element={<AccountInfoFinder />} />
-      <Route path="/cart/myCart" element={<CartPage />} />
-      <Route path="/order" element={<OrderPayPage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/mypage/account" element={<Mypage />} />
-      <Route path="/mypage/purchase-history" element={<PurchaseHistory />} />
-      <Route path="/mypage/likes" element={<MyLikes />} />
-      <Route path="/mypage/reviews" element={<MyReviews />} />
-      <Route path="/mypage/questions" element={<Questions />} />
-    </Routes>
-  </div>
+  <AuthProvider>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="main/category/:categoryId" element={<Categorypage />} />
+        <Route path="/detail/:productid" element={<Detailpage />} />
+        <Route path="/auth/login" element={<LoginPage />} />
+        <Route path="/find-email" element={<AccountInfoFinder />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/order" element={<OrderPayPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage/account" element={<Mypage />} />
+        <Route path="/mypage/purchase-history" element={<PurchaseHistory />} />
+        <Route path="/mypage/likes" element={<MyLikes />} />
+        <Route path="/mypage/reviews" element={<MyReviews />} />
+        <Route path="/mypage/questions" element={<Questions />} />
+      </Routes>
+    </div>
+  </AuthProvider>
 );
 
 export default App;
--- src/components/Header/UserActions.tsx
+++ src/components/Header/UserActions.tsx
@@ -5,22 +5,27 @@ import HeaderLogout from "../../assets/png/power-off.png";
 import HeaderMyPage from "../../assets/png/mypage.png";
 import HeaderCart from "../../assets/png/cart.png";
 import "./Header.css";
+import { useAuth } from "../../contexts/AuthContext";
 
 interface UserActionsProps {
   cartItemCount: number;
   isLoggedIn: boolean;
-  onLogout: () => void;
 }
 
-function UserActions({
-  cartItemCount,
-  isLoggedIn,
-  onLogout,
-}: UserActionsProps) {
+function UserActions({ cartItemCount, isLoggedIn }: UserActionsProps) {
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    const userConfirmed = window.confirm("로그아웃하시겠습니까?");
+    if (userConfirmed) {
+      logout();
+    }
+  };
+
   return (
     <div className="user-actions">
       {isLoggedIn ? (
-        <button className="logout_btn" onClick={onLogout}>
+        <button className="logout_btn" onClick={handleLogout}>
           <img src={HeaderLogout} alt="로그아웃" />
         </button>
       ) : (
@@ -31,7 +36,7 @@ function UserActions({
       <a href="/mypage/account">
         <img src={HeaderMyPage} alt="마이페이지" />
       </a>
-      <a href="/cart/myCart">
+      <a href="/cart">
         <img src={HeaderCart} alt="장바구니" className="useractiocart" />
         {cartItemCount > 0 && (
           <span className="cart-item-count">{cartItemCount}</span>
