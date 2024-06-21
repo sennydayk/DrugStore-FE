c409981ff29e8c600dbef6f6a7caa6b4a2ae42cf
Seongseri <tjdtpfl@gmail.com>
2024-06-22 02:29:38 +0900

    fix: 장바구니 API 500 에러 해결

--- src/pages/Mainpage/Mainpage.tsx
+++ src/pages/Mainpage/Mainpage.tsx
@@ -1,100 +1,105 @@
-import React, { useEffect, useState } from 'react';
-import { Product } from './Product'
-import Header from '../../components/Header/Header';
-import './Mainpage.css'
-import ImageSlider from '../../components/ImageSlider/ImageSlider'
-import axios from 'axios'
-
-
+import React, { useEffect, useState } from "react";
+import { Product } from "./Product";
+import Header from "../../components/Header/Header";
+import "./Mainpage.css";
+import ImageSlider from "../../components/ImageSlider/ImageSlider";
+import axios from "axios";
 
 interface ProductType {
-    product_id: number;
-    brand_name: string;
-    product_name: string;
-    price: number;
-    product_img: string;
-    final_price: number;
-    likes: boolean;
-    sales: boolean;
-    best: boolean;
+  product_id: number;
+  brand_name: string;
+  product_name: string;
+  price: number;
+  product_img: string;
+  final_price: number;
+  likes: boolean;
+  sales: boolean;
+  best: boolean;
 }
 
 interface photosType {
-    src: string;
+  src: string;
 }
 
 const adphotos: photosType[] = [
-    { src: 'https://image.oliveyoung.co.kr/uploads/images/display/90000010001/1/7210969543105371656.jpg' },
-    { src: 'https://image.oliveyoung.co.kr/uploads/images/display/90000010001/1/8563569463792172793.jpg' },
-    { src: 'https://image.oliveyoung.co.kr/uploads/images/display/90000010001/1/7417679956940728494.jpg' },
+  {
+    src: "https://image.oliveyoung.co.kr/uploads/images/display/90000010001/1/7210969543105371656.jpg",
+  },
+  {
+    src: "https://image.oliveyoung.co.kr/uploads/images/display/90000010001/1/8563569463792172793.jpg",
+  },
+  {
+    src: "https://image.oliveyoung.co.kr/uploads/images/display/90000010001/1/7417679956940728494.jpg",
+  },
 ];
 
 const Mainpage = () => {
+  const [Keyword, setKeyword] = useState("");
+  const [Search, setSearch] = useState(false);
+  const [productarray, setProductarray] = useState<ProductType[]>([]);
 
-    const [Keyword, setKeyword] = useState("");
-    const [Search, setSearch] = useState(false);
-    const [productarray, setProductarray] = useState<ProductType[]>([]);
-
-    useEffect(() => {
-        if (Search) {
-            if (Keyword) {
-                getSearchdata(Keyword);
-            } else {
-                getMainpagedata();
-            }
-        }
-        else {
-            getMainpagedata();
-        }
-    }, [Search, Keyword]);
-
-    //mainpage api 가져오기
-    const getMainpagedata = async () => {
-        const token = sessionStorage.getItem('token');
-        console.log('token', token)
-        try {
-            const response = await axios("https://drugstoreproject.shop/main/", {
-                method: "GET",
-                // headers: token ? { Authorization: `Bearer ${token}` } : {}
-                // headers: {
-                //     "Token": token ? sessionStorage.getItem('token') : '',
-                // }
-            });
-            setProductarray(response.data.data.product_list);
-            console.log(productarray)
-        } catch (error) {
-            console.error("데이터 가져오기 중 오류 발생:", error);
-        };
+  useEffect(() => {
+    if (Search) {
+      if (Keyword) {
+        getSearchdata(Keyword);
+      } else {
+        getMainpagedata();
+      }
+    } else {
+      getMainpagedata();
     }
+  }, [Search, Keyword]);
 
-    console.log('productarray', productarray)
+  //mainpage api 가져오기
+  const getMainpagedata = async () => {
+    const token = sessionStorage.getItem("token");
+    console.log("token", token);
+    try {
+      const response = await axios("https://drugstoreproject.shop/main/", {
+        method: "GET",
+        // headers: token ? { Authorization: `Bearer ${token}` } : {}
+        // headers: {
+        //     "Token": token ? sessionStorage.getItem('token') : '',
+        // }
+      });
+      setProductarray(response.data.data.product_list);
+      console.log(productarray);
+    } catch (error) {
+      console.error("데이터 가져오기 중 오류 발생:", error);
+    }
+  };
 
-    //검색 api 가져오기
-    const getSearchdata = async (Keyword: string) => {
-        try {
-            const response = await fetch(`https://drugstoreproject.shop/find?keyword=${Keyword}`, { method: "GET" });
-            const data = await response.json();
-            console.log('data', data)
-            setProductarray(data.data.content);
-        } catch (error) {
-            console.error("데이터 가져오기 중 오류 발생:", error);
-        }
-    };
+  console.log("productarray", productarray);
 
-    // getMainpagedata();
-    console.log('xx', productarray)
-    return (
-        <div>
-            <div className='mainpage_imageslider'>
-                <ImageSlider adphotos={adphotos}></ImageSlider>
-            </div>
-            <div className='mainpage_productlist'>
-                {productarray.map((product, index) => {
-                    return <Product {...product} index={index}></Product>
-                })}
-            </div>
-        </div>
-    );
+  //검색 api 가져오기
+  const getSearchdata = async (Keyword: string) => {
+    try {
+      const response = await fetch(
+        `https://drugstoreproject.shop/find?keyword=${Keyword}`,
+        { method: "GET" }
+      );
+      const data = await response.json();
+      console.log("data", data);
+      setProductarray(data.data.content);
+    } catch (error) {
+      console.error("데이터 가져오기 중 오류 발생:", error);
+    }
+  };
+
+  // getMainpagedata();
+  console.log("xx", productarray);
+  return (
+    <div>
+      <div className="mainpage_imageslider">
+        <ImageSlider adphotos={adphotos}></ImageSlider>
+      </div>
+      <div className="mainpage_productlist">
+        {productarray.map((product, index) => {
+          return <Product {...product} index={index}></Product>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Mainpage;
--- src/pages/ShopCart/ShopCart.tsx
+++ src/pages/ShopCart/ShopCart.tsx
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ShopCart.css";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import Modal from "./CartOpionModal";
 import AlertAlram from "../../assets/png/alert_alram.png";
-import CartTable from "./CartTable";
-import CartSummary from "./CartSummary";
-import CartControls from "./CartControls";
 
 // item 객체의 타입을 정의합니다.
 interface Item {
-  id: number;
-  brand: string;
-  name: string;
-  img: string;
-  orgPrice: number;
-  purPrice: number;
+  product_id: number;
+  cart_id: number;
+  brand_name: string;
+  product_name: string;
+  product_img: string;
+  price: number;
+  f_price: number;
   delivery: string;
   quantity?: number;
   option?: string;
@@ -24,19 +22,19 @@ interface Item {
 const Cart: React.FC = () => {
   let navigate = useNavigate();
   const [items, setItems] = useState<Item[]>([]);
-  const goToOrderForm = () => {
-    navigate("/order");
-  };
   const [currentItem, setCurrentItem] = useState<any>(null);
   const [selectAll, setSelectAll] = useState<boolean>(false);
   const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [totalDeliveryFee, setTotalDeliveryFee] = useState(0);
-  const [finalPrice, setFinalPrice] = useState(0);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [totalDeliveryFee, setTotalDeliveryFee] = useState<number>(0);
+  const [finalPrice, setFinalPrice] = useState<number>(0);
+  const goToOrderForm = () => {
+    navigate("/order");
+  };
   const [itemOptions, setItemOptions] = useState(
     items.map((item) => ({
-      id: item.id,
+      id: item.product_id,
       quantity: 1,
       option: "",
     }))
@@ -48,26 +46,29 @@ const Cart: React.FC = () => {
       if (!token) {
         throw new Error("토큰이 없습니다. 로그인이 필요합니다.");
       }
-
-      const response = await axios.get(
-        "https://drugstoreproject.shop/cart/myCart",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      console.log("사용할 토큰:", token);
+      const config = {
+        method: "get", // HTTP 메서드
+        url: "https://drugstoreproject.shop/cart",
+        headers: {
+          "Content-Type": "application/json",
+          Token: token,
+        },
+      };
+      const response = await axios(config);
+      console.log("서버 응답 데이터:", response.data);
       setItems(response.data.data);
+      setCheckedItems(new Array(response.data.data.length).fill(false));
     } catch (error) {
-      if (error instanceof AxiosError) {
-        console.error("장바구니 데이터를 가져오는 중 오류 발생:", error);
+      if (axios.isAxiosError(error)) {
+        console.error(
+          "장바구니 데이터를 가져오는 중 오류 발생:",
+          error.message
+        );
         if (error.response) {
           console.error("서버 응답 데이터:", error.response.data);
           console.error("서버 응답 상태 코드:", error.response.status);
         }
-      } else if (error instanceof Error) {
-        console.error(error.message);
       } else {
         console.error("알 수 없는 오류 발생:", error);
       }
@@ -76,41 +77,47 @@ const Cart: React.FC = () => {
 
   const addItemToCart = async (item: Item) => {
     try {
-      const response = await axios.post(
-        "https://drugstoreproject.shop/cart/add",
-        item
-      );
-      // 성공적으로 추가되면 장바구니 항목을 다시 가져옵니다.
-      fetchCartItems();
-    } catch (error) {
-      const axiosError = error as AxiosError;
-      console.error("장바구니에 항목을 추가하는 중 오류 발생:", axiosError);
-      if (axiosError.response) {
-        console.error("서버 응답 데이터:", axiosError.response.data);
-        console.error("서버 응답 상태 코드:", axiosError.response.status);
+      const token = sessionStorage.getItem("token");
+      if (!token) {
+        throw new Error("토큰이 없습니다. 로그인이 필요합니다.");
       }
+      const config = {
+        method: "post",
+        url: "http://drugstoreproject.shop/cart",
+        headers: {
+          "Content-Type": "application/json",
+          Token: token,
+        },
+        data: item,
+      };
+      const response = await axios(config);
+      console.log("항목 추가 응답 데이터:", response.data);
+      fetchCartItems(); // 장바구니 항목 다시 불러오기
+    } catch (error) {
+      console.error("항목 추가 중 오류 발생:", error);
     }
   };
 
-  const updateItemInCart = async (
-    id: number,
-    quantity: number,
-    option: string
-  ) => {
+  const updateCartItem = async (cartId: number, updatedItem: Partial<Item>) => {
     try {
-      const response = await axios.put(
-        "https://drugstoreproject.shop/cart/update",
-        { id, quantity, option }
-      );
-      // 성공적으로 업데이트되면 장바구니 항목을 다시 가져옵니다.
-      fetchCartItems();
-    } catch (error) {
-      const axiosError = error as AxiosError;
-      console.error("장바구니 항목을 업데이트하는 중 오류 발생:", axiosError);
-      if (axiosError.response) {
-        console.error("서버 응답 데이터:", axiosError.response.data);
-        console.error("서버 응답 상태 코드:", axiosError.response.status);
+      const token = sessionStorage.getItem("token");
+      if (!token) {
+        throw new Error("토큰이 없습니다. 로그인이 필요합니다.");
       }
+      const config = {
+        method: "put",
+        url: `http://drugstoreproject.shop/cart/${cartId}`,
+        headers: {
+          "Content-Type": "application/json",
+          Token: token,
+        },
+        data: updatedItem,
+      };
+      const response = await axios(config);
+      console.log("항목 수정 응답 데이터:", response.data);
+      fetchCartItems(); // 장바구니 항목 다시 불러오기
+    } catch (error) {
+      console.error("항목 수정 중 오류 발생:", error);
     }
   };
 
@@ -123,15 +130,15 @@ const Cart: React.FC = () => {
   }, [selectAll]);
 
   const handleItemChange = (index: number) => {
-    setCheckedItems((prev) =>
-      prev.map((item, i) => (i === index ? !item : item))
-    );
+    const newCheckedItems = [...checkedItems];
+    newCheckedItems[index] = !newCheckedItems[index];
+    setCheckedItems(newCheckedItems);
   };
 
   useEffect(() => {
     const total = items.reduce((acc, item, index) => {
       if (checkedItems[index]) {
-        return acc + item.purPrice * (item.quantity || 1);
+        return acc + item.f_price * (item.quantity || 1);
       }
       return acc;
     }, 0);
@@ -146,31 +153,46 @@ const Cart: React.FC = () => {
       }
       return acc;
     }, 0);
-
     setTotalPrice(total);
     setTotalDeliveryFee(deliveryFee);
     setFinalPrice(total + deliveryFee);
   }, [checkedItems, items]);
 
-  const handleDeleteItem = (id: number) => {
-    // `id`를 기반으로 해당 아이템을 제외한 새 배열 생성
-    const filteredItems = items.filter((item) => item.id !== id);
-    setItems(filteredItems); // 상태 업데이트
-
-    // 체크된 아이템 상태도 업데이트
-    const updatedCheckedItems = checkedItems.filter(
-      (_, index) => index !== items.findIndex((item) => item.id === id)
-    );
-    setCheckedItems(updatedCheckedItems);
+  const handleDeleteItem = async (id: number) => {
+    try {
+      await axios.delete(`http://drugstoreproject.shop/cart/${id}`);
+      // 아이템을 삭제한 후, 상태를 업데이트
+      setItems((prevItems) =>
+        prevItems.filter((item) => item.product_id !== id)
+      );
+      setCheckedItems((prevChecked) =>
+        prevChecked.filter(
+          (_, index) =>
+            index !== items.findIndex((item) => item.product_id === id)
+        )
+      );
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
   };
 
-  const handleDeleteSelected = () => {
-    // 체크되지 않은 아이템들만 필터링
-    const remainingItems = items.filter((item, index) => !checkedItems[index]);
-    setItems(remainingItems); // 상태 업데이트
-
-    // 체크된 상태도 업데이트
-    setCheckedItems(checkedItems.filter((checked) => checked === false));
+  const handleDeleteSelected = async () => {
+    try {
+      const selectedItems = items.filter((_, index) => checkedItems[index]);
+      const deleteRequests = selectedItems.map((item) =>
+        axios.delete(`http://drugstoreproject.shop/cart/${item.product_id}`)
+      );
+      await Promise.all(deleteRequests);
+      // 선택된 아이템 삭제 후, 상태 업데이트
+      setItems((prevItems) =>
+        prevItems.filter((_, index) => !checkedItems[index])
+      );
+      setCheckedItems((prevChecked) =>
+        prevChecked.filter((checked) => !checked)
+      );
+    } catch (error) {
+      console.error("Error deleting selected items:", error);
+    }
   };
 
   const handleSaveOptions = (id: number, quantity: number, option: string) => {
@@ -181,14 +203,14 @@ const Cart: React.FC = () => {
     );
     setItems((prevItems) =>
       prevItems.map((item) =>
-        item.id === id
+        item.product_id === id
           ? {
               ...item,
               quantity,
               option,
-              name: `${item.name} (${option})`,
-              purPrice: (item.purPrice / (item.quantity || 1)) * quantity,
-              orgPrice: (item.orgPrice / (item.quantity || 1)) * quantity,
+              name: `${item.product_name} (${option})`,
+              purPrice: (item.f_price / (item.quantity || 1)) * quantity,
+              orgPrice: (item.price / (item.quantity || 1)) * quantity,
             }
           : item
       )
@@ -198,7 +220,7 @@ const Cart: React.FC = () => {
   // openModal 함수의 매개변수 item에 Item 타입을 지정합니다.
   const openModal = (item: Item) => {
     const currentItemOptions = itemOptions.find(
-      (option) => option.id === item.id
+      (option) => option.id === item.product_id
     ) || { quantity: 1, option: "" };
     setCurrentItem({ ...item, ...currentItemOptions });
     setIsModalOpen(true);
@@ -211,27 +233,132 @@ const Cart: React.FC = () => {
 
   return (
     <div className="container">
-      <h1 className="shopcart_title">장바구니</h1>
-      <h3 className="shopcart_subtitle">배송상품</h3>
       <div className="cart_wrap">
-        <CartControls
-          selectAll={selectAll}
-          setSelectAll={setSelectAll}
-          handleDeleteSelected={handleDeleteSelected}
-          goToOrderForm={goToOrderForm}
-        />
-        <CartTable
-          items={items}
-          checkedItems={checkedItems}
-          handleItemChange={handleItemChange}
-          handleDeleteItem={handleDeleteItem}
-          openModal={openModal}
-        />
-        <CartSummary
-          totalPrice={totalPrice}
-          totalDeliveryFee={totalDeliveryFee}
-          finalPrice={finalPrice}
-        />
+        <h1 className="shopcart_title">장바구니</h1>
+        <h3>배송상품</h3>
+        <div className="cart-check">
+          <div className="checkbox-label-group">
+            <input
+              type="checkbox"
+              checked={selectAll}
+              onChange={() => setSelectAll(!selectAll)}
+              id="inp_allRe1"
+              title="올리브영 배송상품 전체 선택"
+            />
+            <label htmlFor="inp_allRe1">전체 선택</label>
+          </div>
+          <button type="button" onClick={handleDeleteSelected}>
+            선택상품 삭제
+          </button>
+        </div>
+        <table className="cart_table">
+          <caption>
+            상품정보, 판매가, 수량, 구매가, 배송정보, 선택으로 이루어진 올리브영
+            배송상품 장바구니 목록 표
+          </caption>
+          <thead>
+            <tr className="table_title">
+              <th></th>
+              <th scope="col">상품정보</th>
+              <th scope="col">수량</th>
+              <th scope="col">상품금액</th>
+              <th scope="col">배송정보</th>
+              <th scope="col">선택</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.length > 0 ? (
+              items.map((item, index) => (
+                <tr key={item.product_id}>
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={checkedItems[index] || false}
+                      onChange={() => handleItemChange(index)}
+                    />
+                  </td>
+                  <td>
+                    <a className="prd_img" href="">
+                      <img src={item.product_img} alt={item.product_name} />
+                    </a>
+                    <a className="prd_name" href="">
+                      <span id="brandName">{item.brand_name}</span>
+                      <p id="goodsName">{item.product_name}</p>
+                    </a>
+                  </td>
+                  <td className="prd_quantity">
+                    <span id="prd_quantity">{item.quantity || 1}</span>
+                  </td>
+                  <td>
+                    <span className="org_price">
+                      <span className="tx_num">
+                        {item.price
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </span>
+                      원
+                    </span>
+                    <span className="pur_price">
+                      <span className="tx_num">{item.f_price}</span>원
+                    </span>
+                  </td>
+                  <td>
+                    <p className="prd_delivery">
+                      <strong id="deliStrongText">
+                        {item.delivery}
+                        <span>도서 · 산간 제외</span>
+                      </strong>
+                    </p>
+                  </td>
+                  <td>
+                    <div className="btn_group">
+                      <button
+                        type="button"
+                        className="btnSmall"
+                        onClick={() => openModal(item)}
+                      >
+                        옵션변경
+                      </button>
+                      <button
+                        type="button"
+                        className="btnSmall delete"
+                        onClick={() => handleDeleteItem(item.product_id)}
+                      >
+                        삭제
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={6}>장바구니가 비어 있습니다.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <div className="basket_price_info">
+          <div className="sum_price">
+            총 상품금액
+            <span className="tx_num2">{totalPrice.toLocaleString()}</span>원
+            <span className="tx_sign plus"> +</span> 배송비
+            <span className="tx_num2">{totalDeliveryFee}</span>원
+          </div>
+          <div className="basket_total_price_info">
+            <div className="basket_total_price_alert">
+              <img src={AlertAlram} />
+              <span className="tx_text">
+                결제 시 쿠폰을 적용 받을 경우 금액이 달라질 수 있습니다.
+              </span>
+            </div>
+            <span className="tx_total_price">
+              총 결제예상금액
+              <span className="tx_price">
+                <span className="tx_num3">{finalPrice}</span>원
+              </span>
+            </span>
+          </div>
+        </div>
         <div className="cart_oreder_btn">
           <button
             type="button"
