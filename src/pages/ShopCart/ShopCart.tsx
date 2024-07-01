6fecd3d09c1998df503e9bbc543a83f6cb90392a
Seongseri <tjdtpfl@gmail.com>
2024-07-01 16:01:09 +0900

    fix: 충돌 해결

--- src/pages/ShopCart/ShopCart.tsx
+++ src/pages/ShopCart/ShopCart.tsx
@@ -4,7 +4,6 @@ import "./ShopCart.css";
 import axios, { AxiosResponse } from "axios";
 import CartOptionModal from "./CartOpionModal";
 import AlertAlram from "../../assets/png/alert_alram.png";
-import Item from "antd/es/list/Item";
 
 interface Item {
   product_id: number;
@@ -164,19 +163,30 @@ const CartItem: React.FC = () => {
     }
   };
 
-  const handleSave = (quantity: number, option: string) => {
+  const handleSave = (
+    options_id: number,
+    quantity: number,
+    options_name: string,
+    optionPrice: number
+  ) => {
     setCurrentItem((prevItem: Item | null) => ({
       ...prevItem,
       quantity,
-      options_name: option,
+      options_name,
+      final_price: prevItem?.final_price
+        ? prevItem.final_price + optionPrice
+        : optionPrice,
     }));
     setIsModalOpen(false);
-
-    // items 상태 업데이트
     setItems((prevItems) =>
       prevItems.map((item) =>
         item.product_id === currentItem?.product_id
-          ? { ...item, quantity, options_name: option }
+          ? {
+              ...item,
+              quantity,
+              options_name,
+              final_price: item.final_price + optionPrice,
+            }
           : item
       )
     );
@@ -268,6 +278,7 @@ const CartItem: React.FC = () => {
                       원
                     </span>
                   </td>
+
                   <td>
                     <p className="prd_delivery">
                       <strong id="deliStrongText">
@@ -351,7 +362,6 @@ const CartItem: React.FC = () => {
           onClose={() => setIsModalOpen(false)}
           item={currentItem}
           onSave={handleSave}
-          product_id={currentItem.product_id}
         />
       )}
     </div>
